6a592f8861af3a57447d5d726e72283479dc24a7
Даниил <131484235+FeT677@users.noreply.github.com>
2024-06-01 14:23:14 +0300

    Update CustomTKinterJapanCalc.py

--- pythonProject_JAPCALC/CustomTKinterJapanCalc.py
+++ pythonProject_JAPCALC/CustomTKinterJapanCalc.py
@@ -6,7 +6,9 @@ from lxml import etree
 from tkinter import BooleanVar
 from PIL import Image
 
+
 #To-Do 
+#В расчете 'проходной/непроходной' сделать запас на 2 месяца и добавить переменную которая вовзращает возраст авто в месяцах.
 #Пофиксить парсер ББР банка (он подтягивает курс на продажу, а не на покупку)
 #Сделать серым текстом автозаполнение полей, в которые нужно что-то вносить самому (чтобы у пользователя был пример ввода)
 #Функция расчета утиль сбора
@@ -15,7 +17,7 @@ from PIL import Image
 #Функция расчета таможенного оформления
 #Функция расчета ацкиза
 
-ctk.set_default_color_theme("green")
+ctk.set_default_color_theme("blue")
 
 def set_variables():
     #Для фрейса CTkFrame (основные данные необходимые для расчетов)
@@ -43,7 +45,7 @@ def set_variables():
     summary_in_jp = None
     summary_in_jp_rub = None
     util_sbor = None
-    custom_rate = None
+    custom_rate = 1
     nds = None
     custom_duty = None
     excise = None
@@ -76,6 +78,8 @@ def set_variables():
 
     global jp_comission_global
     jp_comission_global = None
+
+set_variables()
 
 #Парсинг сайта ББР банка для получения курса JPY
 def bbr_pars():
@@ -139,14 +143,14 @@ def auto_fill():
     yen_swift_rate_entry.insert(0,(round(bbr_final_final_pars, 4)))
 
 #Удаление всех виджетов во втором фрейме. Используется в функции смены ЮР/ФИЗ лицо  
-def clear_frame(CTkFrame2):
+def clear_frame():
     for widget in CTkFrame2.winfo_children():
         widget.destroy()   
 
 #Создаем поля с информацией под ЮР ЛИЦО      
 def legal_checkbox_ON():
 
-    clear_frame(CTkFrame2)
+    clear_frame()
 
     global auc_vehicle_price_entry, freight_entry, fob_entry, japan_comission_entry, bank_comission_entry, summary_in_jp_entry, summary_in_jp_rub_entry, \
     util_sbor_entry, custom_rate_entry, nds_entry, custom_duty_entry, excise_entry, custom_services_entry, broker_and_glonas_entry, \
@@ -240,15 +244,10 @@ def legal_checkbox_ON():
     customs_charges_entry = ctk.CTkEntry(CTkFrame2)
     customs_charges_entry.grid(row=11, column=1, padx=10, pady=4, sticky='w')
 
-    custom_services_lable = ctk.CTkLabel(CTkFrame2, text='Брокер + Глонас')
-    custom_services_lable.grid(row=15, column=0, padx=10, pady=4, sticky='w')
-    custom_services_entry = ctk.CTkEntry(CTkFrame2)
-    custom_services_entry.grid(row=15, column=1, padx=10, pady=4, sticky='w')
-
 #Создаем поля с информацией под ЮР ЛИЦО 
 def legal_checkbox_OFF():
 
-    clear_frame(CTkFrame2)
+    clear_frame()
 
     global fiz_auc_vehicle_price_entry, fiz_freight_entry, fiz_fob_entry, fiz_japan_comission_entry, fiz_bank_comission_entry, fiz_summary_in_jp_entry, fiz_summary_in_jp_rub_entry, fiz_util_sbot_entry, \
     fiz_custom_duty_entry, fiz_custom_rate_entry, fiz_custom_services_entry, fiz_sbkts_entry, fiz_svh_entry, fiz_svh_lab_parking_entry, fiz_broker_services_entry, fiz_temp_registrarion_entry, fiz_glonas_entry, \
@@ -370,29 +369,27 @@ def on_legal_checkbox():
     else:
         legal_checkbox_OFF() 
 
-# Функция вычисления возраста авто в Юридических категориях таможни РФ 
-def age_rank():
-    vehicle_month_entryed = int(vehicle_month_entry.get())
-    vehicle_year_entryed = int(vehicle_year_entry.get())
-    current_month_entryed = int(current_month_entry.get())
-    current_year_entryed = int(current_year_entry.get())
-
-    vehicle_date_in_months = (vehicle_year_entryed*12)+vehicle_month_entryed
-    current_date_in_months = (current_year_entryed*12)+current_month_entryed
+# ПЕРЕПИСАТЬ
+def legal_age_rank_check():
+    ...
+# ПЕРЕПИСАТЬ 
+def fiz_age_rank_check():
+    ...
     
-    vehicle_age_months = current_date_in_months - vehicle_date_in_months
-
-    if vehicle_age_months <= 36:
-        age_rank = 'Категория возраста авто: до 3-х лет'
-    elif vehicle_age_months >=36 and vehicle_age_months <= 60:
-        age_rank = 'Категория возраста авто:3-5 лет'
-    elif vehicle_age_months >=60 and vehicle_age_months <= 84:
-        age_rank = 'Категория возраста авто: 5-7 лет'
-    elif vehicle_age_months >=84:
-        age_rank = 'Категория возраста авто: 7+ лет'
-
-    vehicle_age_CTkLabel = ctk.CTkLabel(CTkFrame, text= f"{age_rank}")
-    vehicle_age_CTkLabel.grid(row=5, column=2, columnspan=2, padx=10, pady=4, sticky='w')
+        # clear_frame()
+        # warning_label = ctk.CTkLabel(CTkFrame, text= 'ПЕРЕХОДНЫЙ МЕСЯЦ ВЫПУСКА АВТО', text_color="#B94547")
+        # warning_label.grid(row=5, column=2, columnspan=2, padx=10, pady=4, sticky='we')
+        # warning_label2 = ctk.CTkLabel(CTkFrame, text= 'РЕКОМЕНДУЕТСЯ РАССМОТРЕТЬ ДРУГОЙ ВАРИАНТ', text_color="#B94547")
+        # warning_label2.grid(row=6, column=2, columnspan=2, padx=10, pady=4, sticky='we')
+        # def destroy():
+        #     warning_label.destroy()
+        #     warning_label2.destroy()
+        
+        # CTkFrame.after(3000, destroy)
+        # if legal_check_var.get():
+        #     legal_checkbox_ON
+        # else:
+        #     legal_checkbox_OFF
 
 # Функция вычисления комиссии Японского брокера за цену на аукционе 
 def japan_comission_for_the_price():
@@ -407,6 +404,283 @@ def japan_comission_for_the_price():
         jp_comission_global = 20000
     else:
         jp_comission_global = 0
+    
+    if legal_check_var.get():
+        japan_comission_entry.delete(0, ctk.END)
+        japan_comission_entry.insert(0, f'{jp_comission_global}')
+    else:
+        fiz_japan_comission_entry.delete(0, ctk.END)
+        fiz_japan_comission_entry.insert(0, f'{jp_comission_global}')
+
+def calculate_legal_custom_rate(): 
+
+    vehicle_month_entryed = int(vehicle_month_entry.get())
+    vehicle_year_entryed = int(vehicle_year_entry.get())
+    current_month_entryed = int(current_month_entry.get())
+    current_year_entryed = int(current_year_entry.get())
+
+    vehicle_date_in_months = (vehicle_year_entryed*12)+vehicle_month_entryed
+    current_date_in_months = (current_year_entryed*12)+current_month_entryed
+    motor_volume = int(motor_volume_entry.get())
+    year_for_legal = 10000
+
+    if (current_date_in_months - 82) > vehicle_date_in_months:
+        year_for_legal = 2016
+    elif (current_date_in_months - 35) < vehicle_date_in_months:
+        year_for_legal = 2023
+    else:
+        year_for_legal = 2018
+
+    if year_for_legal < 2017:
+        if motor_volume < 1100:
+            custom_rate = 1.4
+        elif motor_volume < 1600:
+            custom_rate = 1.5
+        else:
+            custom_rate = 1.6
+    else:
+        custom_rate = 0
+
+    custom_rate_entry.delete(0, ctk.END)
+    custom_rate_entry.insert(0, (custom_rate))
+
+def calculate_excise():
+    horse_power = int(horse_power_entry.get())
+
+    if horse_power < 150:
+        if horse_power > 91:
+            excice = (55 * horse_power)
+        else:
+            excice =  0
+    else:
+        excice =  (531 * horse_power)
+    
+    excise_entry.delete(0, ctk.END)
+    excise_entry.insert(0, (excice))
+
+#ПЕРЕПИСАТЬ
+def calculate_duty_for_fiz_entity():
+    ...
+    
+#ПЕРЕПИСАТЬ
+def calculate_duty_for_legal_entity():
+    vehicle_month_entryed = int(vehicle_month_entry.get())
+    vehicle_year_entryed = int(vehicle_year_entry.get())
+    current_month_entryed = int(current_month_entry.get())
+    current_year_entryed = int(current_year_entry.get())
+    vcustom_rate = float(custom_rate_entry.get())
+    vehicle_date_in_months = (vehicle_year_entryed * 12) + vehicle_month_entryed
+    current_date_in_months = (current_year_entryed * 12) + current_month_entryed
+    motor_volume = int(motor_volume_entry.get())
+    year_for_legal = 10000
+    
+    if (current_date_in_months - 82) > vehicle_date_in_months:
+        year_for_legal = 2016
+    elif (current_date_in_months - 35) < vehicle_date_in_months:
+        year_for_legal = 2023
+    else:
+        year_for_legal = 2018
+    car_price_yen = float(vehicle_auc_price_entry.get())
+    euro_rate_for_duty_legal = float(euro_rate_entry.get())
+    yen_rate_for_duty_legal = float(yen_central_bank_rate_entry.get())
+    print(year_for_legal)
+
+    if year_for_legal < 2021:
+        if year_for_legal < 2017:
+            duty_for_legal = round((vcustom_rate * motor_volume * euro_rate_for_duty_legal), 2)
+        else:
+            euro_based_duty = 0.36 * motor_volume * euro_rate_for_duty_legal
+            yen_based_duty = (car_price_yen  + 175000) * yen_rate_for_duty_legal * 0.2
+            duty_for_legal = round((min(euro_based_duty, yen_based_duty)), 2)
+            print(euro_based_duty, yen_based_duty, duty_for_legal )
+    else:
+        duty_for_legal = "Нельзя <3 лет"
+    
+    custom_duty_entry.delete(0, ctk.END)
+    custom_duty_entry.insert(0, duty_for_legal) 
+
+def calculate_util_sbor():
+    motor_volume = float(motor_volume_entry.get())
+    if legal_check_var.get():
+        if motor_volume > 2000:
+            result = 1279
+        elif motor_volume > 1000:
+            result = 529000
+        else:
+            result = 207000
+        util_sbor_entry.delete(0, ctk.END)
+        util_sbor_entry.insert(0, result)
+    else:
+        vehicle_month_entryed = int(vehicle_month_entry.get())
+        vehicle_year_entryed = int(vehicle_year_entry.get())
+        current_month_entryed = int(current_month_entry.get())
+        current_year_entryed = int(current_year_entry.get())
+        vehicle_date_in_months = (vehicle_year_entryed * 12) + vehicle_month_entryed
+        current_date_in_months = (current_year_entryed * 12) + current_month_entryed
+
+        b41 = current_date_in_months
+        b42 = vehicle_date_in_months
+
+        if (b41 - 58) > b42:
+            yearr = 2015
+        elif (b41 - 35) < b42:
+            yearr = 2023
+        else:
+            yearr = 2020
+        
+        if yearr < 2021:
+            result = 5200
+        else:
+            result = 3400
+
+        fiz_util_sbot_entry.delete(0, ctk.END)
+        fiz_util_sbot_entry.insert(0, result)
+#переписать
+def calculate_fiz_custom_rate():
+    ...
+    
+def calculate_bank_fee():
+    def safe_float(entry):
+        value = entry.get().strip()
+        return float(value) if value else 0.0
+
+    if legal_check_var.get():
+        # Получаем значения до любых изменений
+        freight_value = freight_entry.get().strip()
+        fob_value = fob_entry.get().strip()
+        japan_comission_value = japan_comission_entry.get().strip()
+
+        # Преобразуем значения в числа, если возможно
+        freightt = float(freight_value) if freight_value else 0.0
+        fobb = float(fob_value) if fob_value else 0.0
+        japan_comission_entryy = float(japan_comission_value) if japan_comission_value else 0.0
+
+        payment_amount_yen = float(vehicle_auc_price_entry.get()) + \
+                             freightt + \
+                             fobb + \
+                             japan_comission_entryy
+        fee = (payment_amount_yen * 0.008 + 4500) * safe_float(yen_swift_rate_entry)
+        fee_final = fee if fee > 7400 else 7400
+        
+        bank_comission_entry.delete(0, ctk.END)
+        bank_comission_entry.insert(0, fee_final)
+    else:
+        # Получаем значения до любых изменений
+        fiz_freight_value = fiz_freight_entry.get().strip()
+        fiz_fob_value = fiz_fob_entry.get().strip()
+        fiz_japan_comission_value = fiz_japan_comission_entry.get().strip()
+
+        # Преобразуем значения в числа, если возможно
+        fiz_freightt = float(fiz_freight_value) if fiz_freight_value else 0.0
+        fiz_fobb = float(fiz_fob_value) if fiz_fob_value else 0.0
+        fiz_japan_comission_entryy = float(fiz_japan_comission_value) if fiz_japan_comission_value else 0.0
+
+        payment_amount_yen = float(vehicle_auc_price_entry.get()) + \
+                             fiz_freightt + \
+                             fiz_fobb + \
+                             fiz_japan_comission_entryy
+        fee = (payment_amount_yen * 0.008 + 4500) * safe_float(yen_swift_rate_entry)
+        fee_final = fee if fee > 7400 else 7400
+        
+        fiz_bank_comission_entry.delete(0, ctk.END)
+        fiz_bank_comission_entry.insert(0, fee_final)
+
+def calculate_nds():
+    total_cost = 0.555
+    car_price_yen = vehicle_auc_price_entry.get()
+    yen_rate = yen_central_bank_rate_entry.get()
+    custom_duty = custom_duty_entry.get()
+    excise = excise_entry.get()
+    custom_clearancee = custom_services_entry.get()
+    
+    if legal_check_var.get():
+        total_cost = (((int(car_price_yen) + 175000) * float(yen_rate)) + int(float(custom_duty)) + int(excise) + int(custom_clearancee)) * 0.2
+
+        nds_entry.delete(0, ctk.END)
+        nds_entry.insert(0, total_cost) 
+
+def custom_clearance():
+    clearance = 1
+    price = int(vehicle_auc_price_entry.get())
+    if legal_check_var.get():
+        if price < 300000:
+            clearance = 775
+        elif price < 700000:
+            clearance = 1550
+        elif price < 1800000:
+            clearance = 3100
+        else:
+            clearance = 8530
+        custom_services_entry.delete(0, ctk.END)
+        custom_services_entry.insert(0, clearance)
+    else:
+        if price < 300000:
+            clearance = 775
+        elif price < 700000:
+            clearance = 1550
+        elif price < 1800000:
+            clearance = 3100
+        else:
+            clearance = 8530
+        fiz_custom_services_entry.delete(0, ctk.END)
+        fiz_custom_services_entry.insert(0, int(clearance))
+
+#Комманда для кнопки которая выводит все расчеты
+def calculate_everything():
+    if legal_check_var.get():
+        auc_vehicle_price = int(vehicle_auc_price_entry.get())
+        auc_vehicle_price_entry.delete(0, ctk.END)
+        auc_vehicle_price_entry.insert(0, auc_vehicle_price)
+        freight_entry.delete(0, ctk.END)
+        freight_entry.insert(0, '60000')
+        fob_entry.delete(0, ctk.END)
+        fob_entry.insert(0, '60000')
+        broker_and_glonas_entry.delete(0, ctk.END)
+        broker_and_glonas_entry.insert(0, '100000')
+        my_commission_entry.delete(0, ctk.END)
+        my_commission_entry.insert(0, '30000')
+        japan_comission_for_the_price()
+        calculate_legal_custom_rate()
+        calculate_excise()
+        calculate_bank_fee()
+        summary_in_jpy = auc_vehicle_price + int(fob_entry.get()) + int(freight_entry.get()) + int(japan_comission_entry.get())
+        summary_in_jp_entry.delete(0, ctk.END)
+        summary_in_jp_entry.insert (0, f'{summary_in_jpy}')
+        summary_in_jp_rub_entry.delete(0, ctk.END)
+        ggg = round(((int(summary_in_jpy) * float(yen_swift_rate_entry.get()))), 2)
+        summary_in_jp_rub_entry.insert(0, ggg)
+        custom_clearance()
+        calculate_duty_for_legal_entity()
+        calculate_nds()
+        calculate_util_sbor()
+        
+
+
+    else:
+        legal_checkbox_OFF()
+        fiz_auc_vehicle_price = int(vehicle_auc_price_entry.get())
+        fiz_auc_vehicle_price_entry.delete(0, ctk.END)
+        fiz_auc_vehicle_price_entry.insert(0, fiz_auc_vehicle_price)
+        fiz_freight_entry.delete(0, ctk.END)
+        fiz_freight_entry.insert(0, '60000')
+        fiz_fob_entry.delete(0, ctk.END)
+        fiz_fob_entry.insert(0, '60000')
+        fiz_my_commission_entry.delete(0, ctk.END)
+        fiz_my_commission_entry.insert(0, '30000')
+        japan_comission_for_the_price()
+        calculate_fiz_custom_rate()
+        calculate_bank_fee()
+        fiz_summary_in_jpy = fiz_auc_vehicle_price + int(fiz_fob_entry.get()) + int(fiz_freight_entry.get()) + int(fiz_japan_comission_entry.get())
+        fiz_summary_in_jp_entry.delete(0, ctk.END)
+        fiz_summary_in_jp_entry.insert(0, f'{fiz_summary_in_jpy}')
+        fiz_summary_in_jp_rub_entry.delete(0, ctk.END)
+        fiz_ggg = round(((int(fiz_summary_in_jpy) * float(yen_swift_rate_entry.get()))), 2)
+        fiz_summary_in_jp_rub_entry.insert(0, fiz_ggg)
+        custom_clearance()
+        calculate_util_sbor()
+        calculate_duty_for_fiz_entity()
+        
+
 
 space = '\u2008\u2008\u2008\u2008\u2008\u2008\u2008'
 current_month = datetime.now().month
@@ -422,12 +696,12 @@ bbr_final_final_pars = (float(bbr_final_pars)/100)
 eur_rate, jpy_rate = get_exchange_rates()
 jpy_rate = float(jpy_rate/100)
 
-window = ctk.CTk(fg_color='#281b22')
+window = ctk.CTk(fg_color='#343535')
 window.title('РАСЧЕТ СТОИМОСТИ ПОКУПКИ/ТАМОЖНИ/ДОСТАВКИ АВТОМОБИЛЯ ИЗ ЯПОНИИ')
 
 window.geometry("755x743")  
 window.resizable(False, False)
-window.iconbitmap(r'icon.ico')
+
 
 # Загрузка изображений для светлого и тёмного режимов
 light_img = Image.open(r"C:\Users\koajl\Desktop\JapanCalc\backplate.jpg")
@@ -522,35 +796,6 @@ yen_central_bank_rate_CTkLabel = ctk.CTkLabel(CTkFrame, text="КУРС JPY ПО 
 yen_central_bank_rate_CTkLabel.grid(row=2, column=2, padx=10, pady=4, sticky='w')
 yen_central_bank_rate_entry = ctk.CTkEntry(CTkFrame)
 yen_central_bank_rate_entry.grid(row=2, column=3, padx=10, pady=4, sticky='w')
-
-#Комманда для кнопки которая выводит все расчеты
-def calculate_everything():
-    age_rank()
-    if legal_check_var.get():
-        auc_vehicle_price = int(vehicle_auc_price_entry.get())
-        auc_vehicle_price_entry.delete(0, ctk.END)
-        auc_vehicle_price_entry.insert(0, auc_vehicle_price)
-        freight_entry.insert(0, '60000')
-        fob_entry.insert(0, '60000')
-        broker_and_glonas_entry.insert(0, '100000')
-        my_commission_entry.insert(0, '30000')
-        japan_comission_for_the_price()
-        japan_comission_entry.insert(0, f'{jp_comission_global}')
-
-
-        
-
-
-
-    else:
-        fiz_auc_vehicle_price = int(vehicle_auc_price_entry.get())
-        fiz_auc_vehicle_price_entry.delete(0, ctk.END)
-        fiz_auc_vehicle_price_entry.insert(0, fiz_auc_vehicle_price)
-        fiz_freight_entry.insert(0, '60000')
-        fiz_fob_entry.insert(0, '60000')
-        fiz_my_commission_entry.insert(0, '30000')
-        japan_comission_for_the_price()
-        fiz_japan_comission_entry.insert(0, f'{jp_comission_global}')
 
 calculate_button = ctk.CTkButton(CTkFrame, text="Расчитать ориентировочную стоимость", command = calculate_everything)
 calculate_button.grid(row=3, column=2, columnspan=2, padx=10, pady=4, sticky='we')
